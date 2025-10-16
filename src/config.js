0a2183f258cceca73a8d89820ec8684874b648c5
AllRounderBoy7 <sahanapraveen2006@gmail.com>
2025-10-16 20:58:34 +0530

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,9 @@
+// 🔑 SUPABASE KEYS AUR ADMIN CREDENTIALS
+
+export const SUPABASE_URL = 'https://fsvuqwssdgninwzbhuny.supabase.co';
+export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzdnVxd3NzZGduaW53emhidW55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MDQxODQsImV4cCI6MjA3NjA4MDE4NH0.vEM4-Ndv-RDAhR7kzyZjmXpBbR7PLEgf5qBxtMZQG5U';
+
+export const ADMIN_CREDENTIALS = {
+  username: 'sameer',
+  password: 'sameer3745'
+};
